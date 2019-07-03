915af4850e6de860e2f3bf844852cb2aa7378a74
Hetal Patel <nphetal1905@gmail.com>
2019-07-02 21:19:54 -0400

    updated js to select player

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,33 +1,78 @@
 var isSelected = false;
+var imgObject = {
+    img1: {
+        name: "Prince",
+        src: "assets/images/PrinceCard.png",
+        healthPoint: 120
+    },
+    img2: {
+        name: "Executioner",
+        src: "assets/images/ExecutionerCard.png",
+        healthPoint: 150
+    },
+    img3: {
+        name: "Witch",
+        src: "assets/images/WitchCard.png",
+        healthPoint: 80
+    },
+    img4: {
+        name: "Wizard",
+        src: "assets/images/WizardCard.png",
+        healthPoint: 100
+    }
 
-$(document).ready(function(){
+}
 
-    function createImgCard(){
+$(document).ready(function () {
+
+    function createImgCard() {
         var newCard = $("<div>");
         var cardH1 = $("<h5>");
         var cardImg = $("<img>");
-        var cardP = $("<p>"); 
+        var cardP = $("<p>");
 
     }
+   
+        $("#img1").on("click", function () {
+            if(isSelected===false){
+                $("#yourCharacterDiv").html("<img src=assets/images/PrinceCard.png class=imgClass >");
+                $("#enemiesDiv").html("<img src=assets/images/ExecutionerCard.png class=imgClass >")
+                $("#enemiesDiv").append("<img src=assets/images/WitchCard.png class=imgClass >")
+                $("#enemiesDiv").append("<img src=assets/images/WizardCard.png class=imgClass >")
+                isSelected==true
+            }
+        });
+        $("#img2").on("click", function () {
+            if(isSelected===false){
+                $("#yourCharacterDiv").html("<img src=assets/images/ExecutionerCard.png class=imgClass >");
+                $("#enemiesDiv").html("<img src=assets/images/PrinceCard.png class=imgClass >")
+                $("#enemiesDiv").append("<img src=assets/images/WitchCard.png class=imgClass >")
+                $("#enemiesDiv").append("<img src=assets/images/WizardCard.png class=imgClass >")
+                isSelected==true
+            }
+        });
+        $("#img3").on("click", function () {
+            if(isSelected===false){
+                $("#yourCharacterDiv").html("<img src=assets/images/WitchCard.png class=imgClass >");
+                $("#enemiesDiv").html("<img src=assets/images/ExecutionerCard.png class=imgClass >")
+                $("#enemiesDiv").append("<img src=assets/images/PrinceCard.png class=imgClass >")
+                $("#enemiesDiv").append("<img src=assets/images/WizardCard.png class=imgClass >")
+                isSelected==true
+            }
+        });
+        $("#img4").on("click", function () {
+            if(isSelected===false){
+                $("#yourCharacterDiv").html("<img src=assets/images/WizardCard.png class=imgClass >");
+                $("#enemiesDiv").html("<img src=assets/images/ExecutionerCard.png class=imgClass >")
+                $("#enemiesDiv").append("<img src=assets/images/PrinceCard.png class=imgClass >")
+                $("#enemiesDiv").append("<img src=assets/images/WitchCard.png class=imgClass >")
+                isSelected==true
+            }
+        });
 
-    $(".imgClass").on("click",function(){
-        alert(this.src);
-       
-        if(isSelected==false){
-            var newImg = $("<img>");
-            newImg.attr('src',this.src);
-            newImg.addClass('card-img-top');
-            newImg.appendTo("#yourCharacterDiv");
-            isSelected=true
-        }
-        else{
-            var newImg = $("<img>");
-            newImg.attr('src',this.src);
-            newImg.addClass('card-img-top');
-            newImg.appendTo("#enemiesDiv");
-            isSelected=false
-        }
-       
-    });
+
+
+    
+    
 
 });
